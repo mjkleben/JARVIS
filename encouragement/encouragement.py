1ef9df3e3a2f4fb72391780b5d2e4090097896f0
Sung Joon Park <sp3bk@virginia.edu>
2018-08-23 11:30:17 -0400

    quotes/encouragement all scraped

--- encouragement/encouragement.py
+++ encouragement/encouragement.py
@@ -7,7 +7,7 @@ import time
 import datetime
 
 
-def getVerses(site):
+def getBibleVerses(site):
     printable = set(string.printable)
     array = []
     source = requests.get(
@@ -21,20 +21,44 @@ def getVerses(site):
     return array
 
 
-def main():
-    all_verses = []
-    sources = [
-        "https://www.biblestudytools.com/topical-verses/inspirational-bible-verses/", "https://www.biblestudytools.com/topical-verses/encouraging-bible-verses/", "https://www.biblestudytools.com/topical-verses/peace-bible-verses/", "https://www.biblestudytools.com/topical-verses/faith-bible-verses/", "https://www.biblestudytools.com/topical-verses/worry-and-anxiety-bible-verses/", "https://www.biblestudytools.com/topical-verses/bible-verses-about-blessings/", "https://www.biblestudytools.com/topical-verses/bible-verses-to-comfort-you/", "https://www.biblestudytools.com/topical-verses/bible-verses-about-protection/", "https://www.biblestudytools.com/topical-verses/forgiveness-bible-verses/", "https://www.biblestudytools.com/topical-verses/strength-bible-verses/"]
-    for source in sources:
-        all_verses.extend(getVerses(source))
+def getMotivQuotes(site):
+    printable = set(string.printable)
+    array = []
+    source = requests.get(
+        site).text
+    soup = BeautifulSoup(source, 'lxml')
 
-    print all_verses
+    for verse in soup.find_all('a'):
+        if verse.text != "" and len(verse.text.split(" ")) > 4:
+            array.append(''.join(filter(lambda x: x in printable, verse.text.encode(
+                "utf-8").replace("\r", "").replace("\n", "").replace("  ", ""))))
+    return array
+
+
+def main():
+    bible_verses = []
+    motiv_quotes = []
+    all = []
+    bible_sources = [
+        "https://www.biblestudytools.com/topical-verses/inspirational-bible-verses/", "https://www.biblestudytools.com/topical-verses/encouraging-bible-verses/", "https://www.biblestudytools.com/topical-verses/peace-bible-verses/", "https://www.biblestudytools.com/topical-verses/faith-bible-verses/", "https://www.biblestudytools.com/topical-verses/worry-and-anxiety-bible-verses/", "https://www.biblestudytools.com/topical-verses/bible-verses-about-blessings/", "https://www.biblestudytools.com/topical-verses/bible-verses-to-comfort-you/", "https://www.biblestudytools.com/topical-verses/bible-verses-about-protection/", "https://www.biblestudytools.com/topical-verses/forgiveness-bible-verses/", "https://www.biblestudytools.com/topical-verses/strength-bible-verses/"]
+    motiv_sources = ["https://www.brainyquote.com/topics/motivational",
+                     "https://www.brainyquote.com/topics/success", "https://www.brainyquote.com/topics/strength", "https://www.brainyquote.com/topics/anger", "https://www.brainyquote.com/topics/alone", "https://www.brainyquote.com/topics/failure"]
+
+    for source in bible_sources:
+        bible_verses.extend(getBibleVerses(source))
+    for source in motiv_sources:
+        motiv_quotes.extend(getMotivQuotes(source))
+
+    all = bible_verses
+    all.extend(motiv_quotes)
+    print all
     # csv_file = open('verses.csv', 'w')
     # csv_writer = csv.writer(csv_file)
 
-    # for verse in array:
-    #     print verse
+    # for verse in bible_verses:
     #     csv_writer.writerow(verse)
 
     # csv_file.close()
+
+
 main()
